cca9b728c34ec46a7c661dee02f5111c2bf88cd0
Joaquin <joagomez.dev@gmail.com>
2024-12-05 14:29:51 -0300

    update Kconfig

--- taskcar-motor-driver/main/main.c
+++ taskcar-motor-driver/main/main.c
@@ -4,25 +4,27 @@
  * SPDX-License-Identifier: CC0-1.0
  */
 
-#include <stdio.h>
-#include "sdkconfig.h"
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "motor_driver.h"
-#include "esp_log.h"
+#include "sdkconfig.h"
+#include <stdio.h>
 
 void app_main(void)
 {
     esp_err_t err = configure_motor_driver();
 
-    if(err!=ESP_OK) {
+    if (err != ESP_OK)
+    {
         vTaskDelay(pdMS_TO_TICKS(1000));
         esp_restart();
     }
 
-
-    for(int i = 0; i < 8192; i++) {
+    for (int i = 0; i <= 8192; i++)
+    {
         motor_write(0, i, true);
+        motor_write(1, 8092 - i, true);
         vTaskDelay(pdMS_TO_TICKS(50));
     }
 }
